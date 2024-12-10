ef59501bda3cc7749f3befd99ddcb85427cbb8ab
William Giammona <wgiammona@gmail.com>
2024-12-10 14:20:09 +0200

    updated pdf

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,22 @@
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="fixed inset-0">
+      <object
+        data="/William_Giammona_Resume.pdf"
+        type="application/pdf"
+        className="w-full h-full"
+      >
+        <p className="p-4">
+          Your browser does not support PDFs.{" "}
+          <a
+            href="/William_Giammona_Resume.pdf"
+            className="text-blue-600 hover:text-blue-800 underline"
+            download
+          >
+            Download the PDF
+          </a>
+        </p>
+      </object>
+    </div>
+  );
 }
